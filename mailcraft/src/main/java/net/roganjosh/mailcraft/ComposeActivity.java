7d8f763a306abd84279f806b09ec477c5856b4e3
James Burke <jim@roganjosh.net>
2017-11-01 14:25:10 +0000

    gmail sending

--- mailcraft/src/main/java/net/roganjosh/mailcraft/ComposeActivity.java
+++ mailcraft/src/main/java/net/roganjosh/mailcraft/ComposeActivity.java
@@ -1,24 +1,49 @@
 package net.roganjosh.mailcraft;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.util.Rfc822Tokenizer;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.android.ex.chips.BaseRecipientAdapter;
+import com.android.ex.chips.RecipientEditTextView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
 import com.nanotasks.BackgroundWork;
 import com.nanotasks.Completion;
 import com.nanotasks.Tasks;
-import com.pchmn.materialchips.ChipsInput;
-import com.pchmn.materialchips.model.Chip;
-import com.pchmn.materialchips.model.ChipInterface;
 
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
@@ -27,11 +52,13 @@ public class ComposeActivity extends AppCompatActivity {
 
     private final String TAG = ComposeActivity.class.getSimpleName();
 
-    ChipsInput mChipsInput;
+    RecipientEditTextView mChipsInput;
 
     private final String[] PERMISSIONS = {android.Manifest.permission.READ_CONTACTS};
 
     private final int READ_CONTACTS_ALLOWED = 234;
+
+    GoogleAccountCredential mCredential;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,42 +66,59 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mChipsInput = (ChipsInput) findViewById(R.id.ci_recipients);
-        mChipsInput.addChipsListener(new ChipsInput.ChipsListener() {
-            @Override
-            public void onChipAdded(ChipInterface chip, int newSize) {
-                // chip added
-                // newSize is the size of the updated selected chip list
-            }
-
-            @Override
-            public void onChipRemoved(ChipInterface chip, int newSize) {
-                // chip removed
-                // newSize is the size of the updated selected chip list
-            }
-
-            @Override
-            public void onTextChanged(CharSequence text) {
-                // text changed
-                Log.d("CA","Text is " + text);
-            }
-
-            @Override
-            public boolean onTextEntered(CharSequence charSequence) {
-                String name = charSequence.toString();
-                if (name.contains("@")) {
-                    String[] parts = name.split("@");
-                    name = parts[0];
-                }
-                mChipsInput.addChip(name, charSequence.toString());
-                return true;
-            }
-        });
+        mChipsInput = (RecipientEditTextView) findViewById(R.id.retv_recipients);
+        mChipsInput.setTokenizer(new Rfc822Tokenizer());
+        BaseRecipientAdapter baseRecipientAdapter = new BaseRecipientAdapter(BaseRecipientAdapter.QUERY_TYPE_EMAIL, this);
+        mChipsInput.setAdapter(baseRecipientAdapter);
         init();
     }
 
     protected void init() {
         prepareContacts();
+        SharedPreferences sharedPreferences = getSharedPreferences("APP", MODE_PRIVATE);
+        String email = sharedPreferences.getString("credential_name", null);
+        if (StringUtils.isNotEmpty(email)) {
+            Log.d(TAG, "email " + email);
+            // Initialize credentials and service object.
+            mCredential = GoogleAccountCredential.usingOAuth2(
+                    getApplicationContext(), Arrays.asList(GmailScopes.GMAIL_COMPOSE))
+                    .setBackOff(new ExponentialBackOff());
+            mCredential.setSelectedAccountName(email);
+            if ((mCredential != null) && (mCredential.getSelectedAccountName() != null)) {
+                Log.d(TAG, "Account credential " + mCredential.getSelectedAccountName());
+
+                HttpTransport transport = AndroidHttp.newCompatibleTransport();
+                JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+                final Gmail service = new Gmail.Builder(transport, jsonFactory, mCredential)
+                        .setApplicationName(getResources().getString(R.string.app_name))
+                        .build();
+                Tasks.executeInBackground(this, new BackgroundWork<Boolean>() {
+                    @Override
+                    public Boolean doInBackground() throws Exception {
+                        sendGmailMessage(service, mCredential);
+                        return true;
+                    }
+                }, new Completion<Boolean>() {
+                    @Override
+                    public void onSuccess(Context context, Boolean result) {
+                        Toast.makeText(ComposeActivity.this, "Sent mail", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(Context context, Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(ComposeActivity.this, "Failed! " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            } else {
+                Log.d(TAG, "No account credential");
+            }
+        } else {
+            Toast.makeText(this, "No previous signin", Toast.LENGTH_LONG);
+            Intent intent = new Intent(this, GoogleAccountActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -86,7 +130,7 @@ public class ComposeActivity extends AppCompatActivity {
     @AfterPermissionGranted(READ_CONTACTS_ALLOWED)
     private void prepareContacts() {
         if (EasyPermissions.hasPermissions(this, PERMISSIONS)) {
-            beginLoadContacts();
+            // do stuff
         } else if (EasyPermissions.somePermissionPermanentlyDenied(this, Arrays.asList(PERMISSIONS))) {
             Toast.makeText(this, "No access to contacts", Toast.LENGTH_SHORT);
         } else {
@@ -94,30 +138,86 @@ public class ComposeActivity extends AppCompatActivity {
         }
     }
 
-    private void beginLoadContacts() {
-        Tasks.executeInBackground(this, new BackgroundWork<List<Chip>>() {
-            @Override
-            public List<Chip> doInBackground() throws Exception {
-                List<Chip> contactList = new ArrayList<>();
-                contactList.add(new Chip("Bob Label", "Bob info"));
-                contactList.add(new Chip("Fred label", "Fred info"));
-                return contactList;
-            }
-        }, new Completion<List<Chip>>() {
-            @Override
-            public void onSuccess(Context context, List<Chip> result) {
-                if (CollectionUtils.isNotEmpty(result)) {
-                    mChipsInput.setFilterableList(result);
-                } else {
-                    Log.d(TAG, "No contacts loaded");
-                }
-            }
+    /*
+     * GMAIL
+     */
 
-            @Override
-            public void onError(Context context, Exception e) {
-                Log.e(TAG, "Failed to load contacts", e);
-            }
-        });
+    public void sendGmailMessage(Gmail service, GoogleAccountCredential credential) {
+        try {
+            MimeMessage mm = createEmail("shops@roganjosh.net", credential.getSelectedAccountName(), "hello", "test");
+            sendMessage(service, "me", mm);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Create a MimeMessage using the parameters provided.
+     *
+     * @param to email address of the receiver
+     * @param from email address of the sender, the mailbox account
+     * @param subject subject of the email
+     * @param bodyText body text of the email
+     * @return the MimeMessage to be used to send email
+     * @throws MessagingException
+     */
+    public static MimeMessage createEmail(String to, String from, String subject, String bodyText) throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+        return email;
+    }
+
+    /**
+     * Create a message from an email.
+     *
+     * @param emailContent Email to be set to raw of message
+     * @return a message containing a base64url encoded email
+     * @throws IOException
+     * @throws MessagingException
+     */
+    public static Message createMessageWithEmail(MimeMessage emailContent)
+            throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+
+    /**
+     * Send an email from the user's mailbox to its recipient.
+     *
+     * @param service Authorized Gmail API instance.
+     * @param userId User's email address. The special value "me"
+     * can be used to indicate the authenticated user.
+     * @param emailContent Email to be sent.
+     * @return The sent message
+     * @throws MessagingException
+     * @throws IOException
+     */
+    public static Message sendMessage(Gmail service, String userId, MimeMessage emailContent)
+            throws MessagingException, IOException, UserRecoverableAuthIOException {
+        Message message = createMessageWithEmail(emailContent);
+        message = service.users().messages().send(userId, message).execute();
+
+        System.out.println("Message id: " + message.getId());
+        System.out.println(message.toPrettyString());
+        return message;
+    }
+
+    public void clickSignIn(View v) {
+        Intent intent = new Intent(this, GoogleAccountActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ mailcraft/src/main/java/net/roganjosh/mailcraft/GoogleAccountActivity.java
@@ -0,0 +1,325 @@
+package net.roganjosh.mailcraft;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.GmailScopes;
+import com.nanotasks.BackgroundWork;
+import com.nanotasks.Completion;
+import com.nanotasks.Tasks;
+
+import java.util.Arrays;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class GoogleAccountActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mStatusTextView;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_account);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Views
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+        // Button listeners
+        /*
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
+        */
+
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestScopes(new Scope(GmailScopes.GMAIL_COMPOSE))
+                .build();
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        // [END build_client]
+
+        // [START customize_button]
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSignIn(view);
+            }
+        });
+        // [END customize_button]
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideProgressDialog();
+    }
+
+    // [START onActivityResult]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    // [END onActivityResult]
+
+    // [START handleSignInResult]
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess() + ", " + result.getStatus());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            SharedPreferences sharedPreferences = getSharedPreferences("APP", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isLogin", true);
+            editor.putString("name", acct.getDisplayName());
+            editor.putString("e_mail",acct.getEmail());
+            editor.putString("ID", acct.getId());
+            final GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                    getApplicationContext(), Arrays.asList(GmailScopes.GMAIL_COMPOSE))
+                    .setBackOff(new ExponentialBackOff());
+            credential.setSelectedAccount(acct.getAccount());
+            Log.d(TAG, "Signed in as " + acct.getDisplayName() + ", " + acct.getEmail() + ", " + credential.getSelectedAccountName());
+            if (credential != null) {
+                editor.putString("credential_name", credential.getSelectedAccountName());
+            }
+            editor.commit();
+            /*
+            final GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                    getApplicationContext(), Arrays.asList(GmailScopes.GMAIL_COMPOSE))
+                    .setBackOff(new ExponentialBackOff());
+            credential.setSelectedAccount(acct.getAccount());
+            if (credential != null) {
+                Log.d(TAG, "Account credential " + credential.getSelectedAccountName());
+
+                HttpTransport transport = AndroidHttp.newCompatibleTransport();
+                JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+                final Gmail service = new Gmail.Builder(transport, jsonFactory, credential)
+                        .setApplicationName(getResources().getString(R.string.app_name))
+                        .build();
+                Tasks.executeInBackground(this, new BackgroundWork<Boolean>() {
+                    @Override
+                    public Boolean doInBackground() throws Exception {
+                        sendGmailMessage(service, credential);
+                        return true;
+                    }
+                }, new Completion<Boolean>() {
+                    @Override
+                    public void onSuccess(Context context, Boolean result) {
+                        Toast.makeText(AltActivity.this, "Sent mail", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(Context context, Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(AltActivity.this, "Failed! " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            } else {
+                Log.d(TAG, "No account credential");
+            }
+            */
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+    // [END handleSignInResult]
+
+    /**
+     * Attempts to set the account used with the API credentials. If an account
+     * name was previously saved it will use that one; otherwise an account
+     * picker dialog will be shown to the user. Note that the setting the
+     * account to use with the credentials object requires the app to have the
+     * GET_ACCOUNTS permission, which is requested here if it is not already
+     * present. The AfterPermissionGranted annotation indicates that this
+     * function will be rerun automatically whenever the GET_ACCOUNTS permission
+     * is granted.
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void startSignIn() {
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.GET_ACCOUNTS)) {
+            signIn();
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    // [START signIn]
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signIn]
+
+    // [START signOut]
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // [START_EXCLUDE]
+                        updateUI(false);
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END signOut]
+
+    // [START revokeAccess]
+    private void revokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // [START_EXCLUDE]
+                        updateUI(false);
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END revokeAccess]
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    public void onClickSignIn(View v) {
+        startSignIn();
+    }
+
+    public void onClickSignOut(View v) {
+        signOut();
+    }
+
+    public void onClickRevoke(View v) {
+        //revokeAccess();
+        Intent intent = new Intent(this, ComposeActivity.class);
+        startActivity(intent);
+    }
+}
